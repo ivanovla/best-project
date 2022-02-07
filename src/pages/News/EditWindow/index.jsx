c8111940217bc87228b9d49d3aee40480dd9d1df
Budnik-vw <budnik095@yandex.ru>
2022-02-07 18:32:30 +0300

    edit

--- src/pages/News/EditWindow/index.jsx
+++ src/pages/News/EditWindow/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './index.css';
+import React, { useEffect, useState } from 'react';
+import Modal from '../ModalWindow';
+import './index.css'
 
-const Edit = ({open, setOpen, children}) => {
+
+const Edit = ({open, setOpen, item}) => {
+    const [name, setName] = useState(item.name);
+    const [src, setSrc] = useState(item.src);
+    const [header, setHeader] = useState(item.header);
+    const [paragraph, setParagraph] = useState(item.paragraph);
+    useEffect(() => {
+        setName(item.name);
+        setSrc(item.src);
+        setHeader(item.header);
+        setParagraph(item.paragraph);
+    },[item])
     return (
-        <div className={open ? "edit active" : "edit"} onClick={() => setOpen(false)}>
-            <div className={open ? "edit_content active" : "edit_content"} onClick={e => e.stopPropagation()}>
-            {children}
-            </div>
-        </div>
+        <Modal active={open} setActive={setOpen}>
+
+          <input className="edit_data"  onChange={e => setName(e.target.value)} value={name}/>
+          <input className="edit_data"  onChange={e => setSrc(e.target.value) }value={src}/>
+          <input className="edit_data"  onChange={e => setHeader(e.target.value)} value={header}/>
+          <input className="edit_data"  onChange={e => setParagraph(e.target.value)} value={paragraph}/>
+
+          <button className="btn_edit">Применить</button>
+          <button className="btn_edit" onClick={() => setOpen(false)}>Отменить</button>
+        
+        </Modal>
     )
 }
 
--- src/pages/News/ModalWindow/index.jsx
+++ src/pages/News/ModalWindow/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 
-const Modal = ({active, setActive, children}) => {
+const Modal = ({active, setActive, children, className}) => { //мы в итоге className где использовали? почему не подсвечивается?
     return (
-        <div className={active ? 'modal active' : 'modal'} onClick={() =>setActive(false)}>
-            <div className={active ? 'modal_content active' : 'modal_content'} onclick={e => e.stopPropagation()}>
+        <div className={active ? 'modal active' : 'modal'}> 
+            <div className={active ? 'modal_content active ' : 'modal_content'} onclick={e => e.stopPropagation()}>
             {children}
             </div>
-        </div>
+        </div> // почему не работал stopPropagation? Убрал onClick={() =>setActive(false)}
     )
 }
 
