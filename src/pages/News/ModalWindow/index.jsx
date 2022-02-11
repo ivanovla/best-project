269a037b8d488e59e53405c02504a957662fa813
Budnik-vw <budnik095@yandex.ru>
2022-02-11 08:32:56 +0300

    edit_window

--- src/pages/News/ModalWindow/index.jsx
+++ src/pages/News/ModalWindow/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
 
-const Modal = ({active, setActive, children, className}) => { //мы в итоге className где использовали? почему не подсвечивается?
+const Modal = ({active, setActive, children}) => { 
     return (
-        <div className={active ? 'modal active' : 'modal'}> 
-            <div className={active ? 'modal_content active ' : 'modal_content'} onclick={e => e.stopPropagation()}>
+        <div className={active ? 'modal active' : 'modal'} onClick={() =>setActive(false)}> 
+            <div className={active ? 'modal_content active ' : 'modal_content'} onClick={e => e.stopPropagation()}>
             {children}
             </div>
-        </div> // почему не работал stopPropagation? Убрал onClick={() =>setActive(false)}
+        </div>
     )
 }
 
